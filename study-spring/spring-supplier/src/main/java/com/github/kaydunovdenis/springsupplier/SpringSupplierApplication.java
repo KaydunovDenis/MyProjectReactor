a05f0cc6fb39c868bcb1bba55590bf4d5447da57
Denis Kaydunov <deniakaydunov@gmail,com>
2022-03-24 13:31:13 +0300

    initial commit for sprig-supplier

---
+++ study-spring/spring-supplier/src/main/java/com/github/kaydunovdenis/springsupplier/SpringSupplierApplication.java
@@ -0,0 +1,13 @@
+package com.github.kaydunovdenis.springsupplier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSupplierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSupplierApplication.class, args);
+    }
+
+}
